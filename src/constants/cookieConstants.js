fac1ab6cda5ea67d9c14ff2773e6ae836ddbc170
Om Komawar <omkomawar222@gmail.com>
2020-01-29 18:52:21 +0530

    added relevant constants

---
+++ src/constants/cookieConstants.js
@@ -0,0 +1,2 @@
+export const USER_DATA_COOKIE= 'USER_DATA_COOKIE';
+export const LANG = 'LANG';
---
+++ src/constants/flashMessageConstants.js
@@ -0,0 +1,11 @@
+export const flashMessageTypes = {
+  ERROR: 'ERROR',
+  INFO: 'INFO',
+  SUCCESS: 'SUCCESS',
+  WARNING: 'WARNING'
+};
+
+export const reducerStates = {
+  SHOW_FLASH_MESSAGE: 'SHOW_FLASH_MESSAGE',
+  HIDE_FLASH_MESSAGE: 'HIDE_FLASH_MESSAGE',
+};
