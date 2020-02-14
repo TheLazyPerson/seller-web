8bc73f53a4cd290a2b489cd2c727c3a1ee504f9a
Om Komawar <omkomawar222@gmail.com>
2020-02-14 22:30:03 +0530

    completed benefits section

--- src/constants/benefitsConstants.js
+++ src/constants/benefitsConstants.js
@@ -5,7 +5,7 @@ import servicesOfDelivery from "Images/services-for-delivery.svg";
 export const benefitsListItems = [
   {
     heroImage: paymentsImage,
-    title: "Secure Payments,Regularly",
+    title: "Secure Payments, Regularly",
     slug: "payments",
     description:
       "Funds are safely deposited directly to your bank account, even for Pay on Delivery orders, every 7 days."
--- src/modules/pages/landingPage/benefits/index.js
+++ src/modules/pages/landingPage/benefits/index.js
@@ -4,26 +4,26 @@ import DivColumn from "CommonComponents/divColumn";
 import DivRow from "CommonComponents/divRow";
 import { benefitsListItems } from "Constants/benefitsConstants";
 import map from "lodash/map";
+import styles from "./benefits.module.scss";
 
 class Benefits extends Component {
   getBenefitsListItem = listItem => {
     return (
-      <DivColumn fillParent>
-        <img src={listItem.heroImage} />
+      <DivColumn fillParent className={styles.list_item}>
+        <img className={styles.image} src={listItem.heroImage} />
         <DivColumn>
-          <div>{listItem.title}</div>
-          <div>{listItem.description}</div>
+          <div className={styles.title}>{listItem.title}</div>
+          <div className={styles.description}>{listItem.description}</div>
         </DivColumn>
       </DivColumn>
     );
   };
   render() {
     return (
-      <DivRow fillParent>
+      <DivRow fillParent className={styles.benefits}>
         {map(benefitsListItems, listItem => {
           return this.getBenefitsListItem(listItem);
         })}
-        ;
       </DivRow>
     );
   }
