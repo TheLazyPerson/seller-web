e1449209d40ccd931065186ebc81be1ddac1b8e6
Om Komawar <omkomawar222@gmail.com>
2020-02-14 22:16:46 +0530

    added benefits component

---
+++ src/constants/benefitsConstants.js
@@ -0,0 +1,27 @@
+import paymentsImage from "Images/payment-image.svg";
+import orderDelivery from "Images/order-delivery-image.svg";
+import servicesOfDelivery from "Images/services-for-delivery.svg";
+
+export const benefitsListItems = [
+  {
+    heroImage: paymentsImage,
+    title: "Secure Payments,Regularly",
+    slug: "payments",
+    description:
+      "Funds are safely deposited directly to your bank account, even for Pay on Delivery orders, every 7 days."
+  },
+  {
+    heroImage: orderDelivery,
+    title: "Ship your orders, stress-free",
+    slug: "orders",
+    description:
+      "Whether you choose Fulfillment by Home Expo or Easy Ship, let us take care of delivering your products."
+  },
+  {
+    heroImage: servicesOfDelivery,
+    title: "Services to help you through every step",
+    slug: "payments",
+    description:
+      "Get paid support from certified third party professionals for product photography, account management and much more"
+  }
+];
--- src/modules/pages/landingPage/benefits/index.js
+++ src/modules/pages/landingPage/benefits/index.js
@@ -1,9 +1,31 @@
 import React, { Component } from "react";
 import navigatorHoc from "Hoc/navigatorHoc";
+import DivColumn from "CommonComponents/divColumn";
+import DivRow from "CommonComponents/divRow";
+import { benefitsListItems } from "Constants/benefitsConstants";
+import map from "lodash/map";
 
 class Benefits extends Component {
+  getBenefitsListItem = listItem => {
+    return (
+      <DivColumn fillParent>
+        <img src={listItem.heroImage} />
+        <DivColumn>
+          <div>{listItem.title}</div>
+          <div>{listItem.description}</div>
+        </DivColumn>
+      </DivColumn>
+    );
+  };
   render() {
-    return <h1>Benefits</h1>;
+    return (
+      <DivRow fillParent>
+        {map(benefitsListItems, listItem => {
+          return this.getBenefitsListItem(listItem);
+        })}
+        ;
+      </DivRow>
+    );
   }
 }
 
