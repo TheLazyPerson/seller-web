38a0516a6acdccce328baade9c2ae014ed1a095a
Om Komawar <omkomawar222@gmail.com>
2020-02-15 07:22:28 +0530

    pricing created a selected subscription plan

--- src/modules/pages/landingPage/Pricing/index.js
+++ src/modules/pages/landingPage/Pricing/index.js
@@ -13,7 +13,10 @@ class Pricing extends Component {
   render() {
     return (
       <DivRow fillParent className={styles.subscription_container}>
-        <DivColumn fillParent className={styles.subscription}>
+        <DivColumn
+          fillParent
+          className={`${styles.subscription} ${styles.is_selected}`}
+        >
           <div className={styles.subscription_title}>Starter</div>
           <div className={styles.subscription_price}>KD 0 / Month</div>
           <div className={styles.subtitle}>Benefits Include:</div>
@@ -52,7 +55,42 @@ class Pricing extends Component {
           </DivColumn>
         </DivColumn>
         <DivColumn fillParent className={styles.subscription}>
-          Subscription 2
+          <div className={styles.subscription_title}>Starter</div>
+          <div className={styles.subscription_price}>KD 0 / Month</div>
+          <div className={styles.subtitle}>Benefits Include:</div>
+          <DivColumn fillParent className={styles.features}>
+            <DivRow className={styles.feature}>
+              <img
+                alt="nav"
+                src={exhibitionIcon}
+                className={styles.feature_icon}
+              />
+              <DivColumn className={styles.feature_details}>
+                <div className={styles.feature_title}>10 Exhibitions</div>
+                <div className={styles.feature_description}>
+                  You can enroll in 10 number of exhibitions
+                </div>
+              </DivColumn>
+            </DivRow>
+            <DivRow className={styles.feature}>
+              <img alt="nav" src={growthIcon} className={styles.feature_icon} />
+              <DivColumn className={styles.feature_details}>
+                <div className={styles.feature_title}>10 Exhibitions</div>
+                <div className={styles.feature_description}>
+                  You can enroll in 10 number of exhibitions
+                </div>
+              </DivColumn>
+            </DivRow>
+            <DivRow className={styles.feature}>
+              <img alt="nav" src={rocketIcon} className={styles.feature_icon} />
+              <DivColumn className={styles.feature_details}>
+                <div className={styles.feature_title}>10 Exhibitions</div>
+                <div className={styles.feature_description}>
+                  You can enroll in 10 number of exhibitions
+                </div>
+              </DivColumn>
+            </DivRow>
+          </DivColumn>
         </DivColumn>
         <DivColumn fillParent className={styles.subscription}>
           Subscription 3
