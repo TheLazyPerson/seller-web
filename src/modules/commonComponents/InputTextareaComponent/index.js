3a6007a3ebb6f3504c5d474a58d43610752274c4
Om Komawar <omkomawar222@gmail.com>
2020-02-16 10:58:58 +0530

    added common text area component

---
+++ src/modules/commonComponents/InputTextareaComponent/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import styles from "./input_textarea_component.module.scss";
+import DivColumn from "CommonComponents/divColumn";
+import tranlatorHoc from "Hoc/translatorHoc";
+
+class InputTextareaComponent extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const { className, type, meta = {}, isRTL, ...rest } = this.props;
+
+    return (
+      <DivColumn
+        className={`${isRTL ? styles.rtl : ""} ${className ? className : ""}`}
+      >
+        <textarea
+          rows="4"
+          {...rest}
+          type={type ? type : "textarea"}
+          className={styles.input_text_area}
+        ></textarea>
+        {meta.error && meta.touched && (
+          <span className={styles.error_text}>{meta.error}</span>
+        )}
+      </DivColumn>
+    );
+  }
+}
+
+export default tranlatorHoc(InputTextareaComponent);
