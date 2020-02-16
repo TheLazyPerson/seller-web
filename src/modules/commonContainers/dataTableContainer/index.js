80dddbcf3629e3491326db6a16214112f8f4619f
Om Komawar <omkomawar222@gmail.com>
2020-02-16 14:55:01 +0530

    updated datatable component to take data from parent containers

--- src/modules/commonContainers/dataTableContainer/index.js
+++ src/modules/commonContainers/dataTableContainer/index.js
@@ -13,67 +13,64 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { CookieService } from "Utils/cookieService";
 import { USER_DATA_COOKIE } from "Constants/cookieConstants";
-import DataTable, { createTheme } from 'react-data-table-component';
-import differenceBy from 'lodash/differenceBy';
-import Card from '@material-ui/core/Card';
-import IconButton from '@material-ui/core/IconButton';
-import Checkbox from '@material-ui/core/Checkbox';
-import ArrowDownward from '@material-ui/icons/ArrowDownward';
-import Delete from '@material-ui/icons/Delete';
-import Add from '@material-ui/icons/Add';
-import memoize from 'memoize-one';
-import SearchBarComponent from 'CommonComponents/searchBarComponent';
+import DataTable, { createTheme } from "react-data-table-component";
+import differenceBy from "lodash/differenceBy";
+import Card from "@material-ui/core/Card";
+import IconButton from "@material-ui/core/IconButton";
+import Checkbox from "@material-ui/core/Checkbox";
+import ArrowDownward from "@material-ui/icons/ArrowDownward";
+import Delete from "@material-ui/icons/Delete";
+import Add from "@material-ui/icons/Add";
+import memoize from "memoize-one";
+import SearchBarComponent from "CommonComponents/searchBarComponent";
 
 const sortIcon = <ArrowDownward />;
 const selectProps = { indeterminate: isIndeterminate => isIndeterminate };
 
 const contextActions = memoize(deleteHandler => (
-  <IconButton
-    color="secondary"
-    onClick={deleteHandler}
-  >
+  <IconButton color="secondary" onClick={deleteHandler}>
     <Delete />
   </IconButton>
 ));
 
 const columns = memoize(() => [
   {
-    name: 'ID',
-    selector: 'id',
-    sortable: true,
+    name: "ID",
+    selector: "id",
+    sortable: true
   },
   {
-    name: 'ORDER DATE',
-    selector: 'order_date',
-    sortable: true,
+    name: "ORDER DATE",
+    selector: "order_date",
+    sortable: true
   },
   {
-    name: 'EXHIBITION NAME',
-    selector: 'exhibition_name',
+    name: "EXHIBITION NAME",
+    selector: "exhibition_name",
     sortable: true,
-    grow: 2,
+    grow: 2
   },
   {
-    name: 'GRAND TOTAL',
-    selector: 'grand_total',
-    sortable: true,
+    name: "GRAND TOTAL",
+    selector: "grand_total",
+    sortable: true
   },
   {
-    name: 'TOTAL ITEMS',
-    selector: 'total_items',
-    sortable: true,
+    name: "TOTAL ITEMS",
+    selector: "total_items",
+    sortable: true
   },
   {
-    name: 'STATUS',
-    selector: 'status',
-    sortable: true,
+    name: "STATUS",
+    selector: "status",
+    sortable: true
   }
 ]);
 
-
 class OrdersPage extends Component {
   state = {
-    selectedRows: [], toggleCleared: false
+    selectedRows: [],
+    toggleCleared: false
   };
 
   handleChange = state => {
@@ -81,22 +78,24 @@ class OrdersPage extends Component {
   };
 
   handleRowClicked = row => {
-
     console.log(`${row.name} was clicked!`);
-  }
+  };
 
   deleteAll = () => {
     const { selectedRows } = this.state;
     const rows = selectedRows.map(r => r.name);
 
     if (window.confirm(`Are you sure you want to delete:\r ${rows}?`)) {
-      this.setState(state => ({ toggleCleared: !state.toggleCleared, data: differenceBy(state.data, state.selectedRows, 'name') }));
+      this.setState(state => ({
+        toggleCleared: !state.toggleCleared,
+        data: differenceBy(state.data, state.selectedRows, "name")
+      }));
     }
-  }
+  };
 
   render() {
     const { toggleCleared } = this.state;
-    const { data } = this.props;
+    const { data, columns, title } = this.props;
 
     return (
       <DivColumn>
@@ -106,8 +105,8 @@ class OrdersPage extends Component {
 
         <Card>
           <DataTable
-            title="Orders"
-            columns={columns()}
+            title={title ? title : "Orders"}
+            columns={columns ? columns : columns()}
             data={data}
             selectableRows
             highlightOnHover
@@ -134,7 +133,4 @@ const mapDispathToProps = dispatch => {
   };
 };
 
-export default connect(
-  null,
-  mapDispathToProps
-)(navigatorHoc(OrdersPage));
+export default connect(null, mapDispathToProps)(navigatorHoc(OrdersPage));
