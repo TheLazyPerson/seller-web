8952e842f83b232dfecf157a1f95613e0ec3df89
Riyaz <ahmed.riyaz94@gmail.com>
2020-02-17 15:34:02 +0530

    created overlay container

--- src/modules/commonContainers/sectionedHeader/index.js
+++ src/modules/commonContainers/sectionedHeader/index.js
@@ -18,32 +18,27 @@ import { searchTypes } from "Constants/searchConstants";
 import { connect } from "react-redux";
 import translatorHoc from "Hoc/translatorHoc";
 import SearchBar from "../searchBar";
+import OverlayContainer from './overlayContainer';
 
 class SectionedHeader extends Component {
   clickedOnSearchItem = false;
 
   state = {
     searchText: "",
-    showSearchResult: false
+    showOverlayComponent: true,
   };
 
   onClickProfile = () => {
-    const { navigateTo } = this.props;
-    navigateTo("profile");
-  };
+    const { showOverlayComponent } = this.state;
 
-  onClickWishlist = () => {
-    const { navigateTo } = this.props;
-    navigateTo("wishlist");
-  };
-
-  onClickBag = () => {
-    const { navigateTo } = this.props;
-    navigateTo("checkout");
+    this.setState({
+      showOverlayComponent: !showOverlayComponent
+    });
   };
 
   render() {
     const { isUserSignedIn, bagCount, whiteColor, translate } = this.props;
+    const { showOverlayComponent } = this.state;
 
     return (
       <DivRow className={styles.header_container}>
@@ -51,30 +46,36 @@ class SectionedHeader extends Component {
           {isUserSignedIn ? (
             <div
               style={{ height: "unset" }}
-              className={`${styles.header_icon} ${styles.header_item_container}`}
-              onClick={this.onClickProfile}
+              className={`${styles.header_icon} ${styles.header_item_container} ${styles.profile_header_container}`}
             >
               <img
                 className={styles.profile_pic}
                 src={whiteColor ? profileIconWhite : profileIconBlack}
+                onClick={this.onClickProfile}
               />
-              {/* <img src={arrowDownIcon} className={styles.arrow_down_icon} /> */}
+              <img src={arrowDownIcon} className={styles.arrow_down_icon} onClick={this.onClickProfile}
+              />
+              {
+                showOverlayComponent ? (
+                  <OverlayContainer />
+                ) : null
+              }
             </div>
           ) : (
-            <a
-              className={`${styles.sigin_link} ${
-                styles.header_item_container
-              } ${whiteColor ? styles.is_white : ""}`}
-              href="/signin"
-            >
-              {translate("header.login")}
-            </a>
-          )}
+              <a
+                className={`${styles.sigin_link} ${
+                  styles.header_item_container
+                  } ${whiteColor ? styles.is_white : ""}`}
+                href="/signin"
+              >
+                {translate("header.login")}
+              </a>
+            )}
           {!isUserSignedIn && (
             <a
               className={`${styles.sigin_link} ${
                 styles.header_item_container
-              } ${whiteColor ? styles.is_white : ""}`}
+                } ${whiteColor ? styles.is_white : ""}`}
               href="/signup"
             >
               {translate("header.register")}
---
+++ src/modules/commonContainers/sectionedHeader/overlayContainer/index.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import styles from './overlay_container.module.scss';
+import HorizontalBorder from 'CommonComponents/horizontalBorder';
+import DivColumn from 'CommonComponents/divColumn';
+import DivRow from 'CommonComponents/divRow';
+
+class OverlayContainer extends Component {
+
+  render() {
+    const ProgressItem = ({ title, value, progress }) => (
+      <DivColumn className={styles.item_container}>
+        <DivRow className={styles.display_container}>
+          <div className={styles.title}>{title}</div>
+          <div className={styles.value}>{value}</div>
+        </DivRow>
+        <DivRow className={styles.progress_bar_container}>
+          <div style={{ width: `${progress}%` }} className={styles.progress_bar_filled}></div>
+        </DivRow>
+      </DivColumn>
+    )
+
+    return (
+      <DivColumn className={styles.overlay_container}>
+
+        <div className={`${styles.item_container} ${styles.title}`}>
+          USAGE
+        </div>
+        <HorizontalBorder />
+
+        <ProgressItem
+          title="EXHIBITIONS USED"
+          value="4/5"
+          progress={50}
+        />
+
+        <HorizontalBorder />
+
+        <ProgressItem
+          title="PRODUCTS USED"
+          value="4/5"
+          progress={50}
+        />
+        <HorizontalBorder />
+
+        <div className={styles.item_container}>
+          Billing
+        </div>
+        <HorizontalBorder />
+
+        <div className={styles.item_container}>
+          Logout
+        </div>
+        <HorizontalBorder />
+      </DivColumn>
+    )
+  }
+}
+
+export default OverlayContainer;
