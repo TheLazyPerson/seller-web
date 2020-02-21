3d1119f40f6535d329a0aa6b630999a5d709cdef
Om Komawar <omkomawar222@gmail.com>
2020-02-21 08:34:08 +0530

    added mapping in product list item

--- src/modules/commonComponents/productListItem/index.js
+++ src/modules/commonComponents/productListItem/index.js
@@ -1,17 +1,21 @@
-import React , { Component } from 'react'
-import DivColumn from 'CommonComponents/divColumn';
-import styles from './product_list_item.module.scss';
-import exhibitionImage from 'Images/exhibition-item-1.jpg';
+import React, { Component } from "react";
+import DivColumn from "CommonComponents/divColumn";
+import styles from "./product_list_item.module.scss";
+import exhibitionImage from "Images/exhibition-item-1.jpg";
 
 class ProductListItem extends Component {
   render() {
+    const { product } = this.props;
     return (
-        <DivColumn horizontalCenter className={styles.product_list_item_container}>
-          <div className={styles.title}>Adidas Red Shoes</div>
-          <div className={styles.description}>Elegant. Timeless. The quitessensial symbol of love</div>
-          <img src={exhibitionImage} className={styles.image}/>
-          <div className={styles.action_button}>Mark Out of Stock</div>
-        </DivColumn>
+      <DivColumn
+        horizontalCenter
+        className={styles.product_list_item_container}
+      >
+        <div className={styles.title}>{product.name}</div>
+        <div className={styles.description}>{product.short_description}</div>
+        <img src={product.base_image.path} className={styles.image} />
+        <div className={styles.action_button}>Mark Out of Stock</div>
+      </DivColumn>
     );
   }
 }
