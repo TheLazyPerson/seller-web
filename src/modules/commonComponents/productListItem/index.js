91b54b913ce43f584c5c4f5cb3d1f52500d4e39d
Om Komawar <omkomawar222@gmail.com>
2020-02-21 11:01:31 +0530

    added attach product action

--- src/modules/commonComponents/productListItem/index.js
+++ src/modules/commonComponents/productListItem/index.js
@@ -3,10 +3,27 @@ import DivColumn from "CommonComponents/divColumn";
 import styles from "./product_list_item.module.scss";
 import exhibitionImage from "Images/exhibition-item-1.jpg";
 import isEmpty from "lodash/isEmpty";
+import { attachProductsToExhibition } from "Core/modules/exhibition/exhibitionActions";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import navigatorHoc from "Hoc/navigatorHoc";
 
 class ProductListItem extends Component {
+  onClickAttachProduct = (exhibitionId, productId) => {
+    const {
+      attachProductsToExhibition,
+      exhibitionReducer: { exhibitionDetail }
+    } = this.props;
+    attachProductsToExhibition(exhibitionId, {
+      products: [productId]
+    }).then(({ exhibitionDetail }) => {
+      console.log("attachment successful");
+    });
+  };
   render() {
-    const { product } = this.props;
+    const { product, exhibitionId, actionType } = this.props;
+    const ACTION_TYPE_ATTACH_PRODUCT = "attach_product";
+    const ACTION_TYPE_MARK_OUT_OF_STOCK = "mark_product_out_of_stock";
     return (
       <DivColumn
         horizontalCenter
@@ -17,11 +34,39 @@ class ProductListItem extends Component {
         {!isEmpty(product.thumbnail) && (
           <img src={product.thumbnail} className={styles.image} />
         )}
-
-        <div className={styles.action_button}>Mark Out of Stock</div>
+        {actionType == ACTION_TYPE_ATTACH_PRODUCT && (
+          <div
+            className={styles.action_button}
+            onClick={() => this.onClickAttachProduct(exhibitionId, product.id)}
+          >
+            ATTACH
+          </div>
+        )}
+        {actionType == ACTION_TYPE_MARK_OUT_OF_STOCK && (
+          <div className={styles.action_button}>MARK PRODUCT OUT OF STOCK</div>
+        )}
       </DivColumn>
     );
   }
 }
 
-export default ProductListItem;
+const mapStateToProps = state => {
+  return {
+    productReducer: state.productReducer,
+    exhibitionReducer: state.exhibitionReducer
+  };
+};
+
+const mapDispathToProps = dispatch => {
+  return {
+    attachProductsToExhibition: bindActionCreators(
+      attachProductsToExhibition,
+      dispatch
+    )
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispathToProps
+)(navigatorHoc(ProductListItem));
