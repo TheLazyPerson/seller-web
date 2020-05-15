d2a193c3e1afb6278e14294fbd48c9b494227759
Om Komawar <omkomawar222@gmail.com>
2020-05-16 00:40:01 +0530

    updated order shipping information page

--- src/modules/pages/ordersShippingInfomationPage/index.js
+++ src/modules/pages/ordersShippingInfomationPage/index.js
@@ -13,16 +13,62 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import DataTableContainer from "CommonContainers/dataTableContainer";
 import DataTable from "react-data-table-component";
-import { getOrderDetailsAction } from "Core/modules/order/orderActions";
+import { showSuccessFlashMessage } from "Redux/actions/flashMessageActions";
+
+import {
+  getOrderDetailsAction,
+  createPickupRequestAction,
+} from "Core/modules/order/orderActions";
 import InitialPageLoader from "CommonContainers/initialPageLoader";
 import HorizontalBorder from "CommonComponents/horizontalBorder";
 import SecondaryCapsuleButton from "CommonComponents/secondaryCapsuleButton";
+import { Form, Field } from "react-final-form";
+import InputTextComponent from "CommonComponents/InputTextComponent";
+import { isEmptyValidator } from "Utils/validators";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 class OrderShippingInformationPage extends Component {
+  state = {
+    startDate: null,
+  };
+
   onBackPress = () => {
     const { pop } = this.props;
     pop();
   };
+
+  validate = (values) => {
+    const errors = {};
+    const validators = {
+      pickupDate: isEmptyValidator(values.pickupDate),
+    };
+
+    Object.keys(validators).forEach((key) => {
+      if (!validators[key].result) errors[key] = validators[key].error;
+    });
+
+    return errors;
+  };
+
+  onSubmit = (form) => {
+    const {
+      createPickupRequestAction,
+      navigateTo,
+      match: { params },
+      showSuccessFlashMessage,
+    } = this.props;
+
+    createPickupRequestAction(params.orderId, {
+      pickup_date: form.pickupDate,
+    }).then(({ payload }) => {
+      if (payload.code === 200 || payload.code === 201) {
+        navigateTo("order-details", { orderId: params.orderId });
+        showSuccessFlashMessage("Pickup Request Created successfuly");
+      }
+    });
+  };
+
   render() {
     const rowStyle = {
       fontSize: 12,
@@ -30,7 +76,7 @@ class OrderShippingInformationPage extends Component {
     };
     const columns = [
       {
-        name: "SKU",
+        name: "ITEM CODE",
         selector: "sku",
         style: rowStyle,
       },
@@ -41,18 +87,8 @@ class OrderShippingInformationPage extends Component {
       },
       {
         name: "EXHIBITION NAME",
-        selector: "exhibition_name",
+        selector: "exhibition.title",
         grow: 2,
-        style: rowStyle,
-      },
-      {
-        name: "STATUS",
-        selector: "status",
-        style: rowStyle,
-      },
-      {
-        name: "PRICE",
-        selector: "price",
         style: rowStyle,
       },
       {
@@ -61,13 +97,8 @@ class OrderShippingInformationPage extends Component {
         style: rowStyle,
       },
       {
-        name: "COMMISSION",
-        selector: "commission",
-        style: rowStyle,
-      },
-      {
-        name: "GRAND TOTAL",
-        selector: "formated_base_total",
+        name: "QUANTITY TO SHIP",
+        selector: "qty_ordered",
         style: rowStyle,
       },
     ];
@@ -89,82 +120,137 @@ class OrderShippingInformationPage extends Component {
         },
       },
     };
+    let startDate = null;
+    if (this.state.startDate) {
+      startDate = this.state.startDate;
+    }
+    const {
+      orderReducer: { order },
+      match: { params },
+      getOrderDetailsAction,
+    } = this.props;
+
+    const CustomRenderInput = ({ input, value, onClick, meta }) => {
+      return (
+        <InputTextComponent
+          {...input}
+          meta={meta}
+          placeholder="Select Pickup Date"
+          value={value}
+          className={styles.input_text}
+          onClick={onClick}
+        />
+      );
+    };
+
     return (
       <SectionedContainer sideBarContainer={<SideNav />}>
         <NavHeader
-          title="Order Detail"
+          title="Create Shipping Request"
           onBackClick={this.onBackPress}
         ></NavHeader>
-
-        <DivColumn className={styles.order_page_container}>
-          <DivColumn className={styles.order_container}>
-            <div className={styles.order_id}>
-              Order ID: <b>123</b>
-            </div>
-            <div className={styles.placed_on}>Placed On: 16 OCT 2019</div>
-            <div className={styles.status}>Delivered</div>
+        <InitialPageLoader
+          initialPageApi={() => getOrderDetailsAction(params.orderId)}
+        >
+          <DivColumn className={styles.order_page_container}>
+            <DivColumn className={styles.order_container}>
+              <div className={styles.order_id}>
+                Order ID: <b>{order.id}</b>
+              </div>
+              <div className={styles.placed_on}>
+                Placed On: {order.created_at}
+              </div>
+              <div className={styles.status}>{order.status_label}</div>
+            </DivColumn>
           </DivColumn>
-        </DivColumn>
-        <div className={styles.header}>SHIPPING INFORMATION</div>
+          <div className={styles.header}>FILL IN THESE DETAILS</div>
+          <Form
+            onSubmit={this.onSubmit}
+            validate={this.validate}
+            // initialValues={{
+            //   iban: bankDetails.iban ? bankDetails.iban : "",
+            // }}
+            //TODO: add time
 
-        <DivColumn className={styles.normal_container}>
-          <DivRow className={styles.title}>
-            <span>Inventory Source:</span>
-            <div className={styles.value}>Omar Lastname</div>
-          </DivRow>
-          <DivRow className={styles.title}>
-            <span>Carrier Title:</span>
-            <div className={styles.value}>omarlastname@mail.com</div>
-          </DivRow>
-          <DivRow className={styles.title}>
-            <span>Tracking Number:</span>
-            <div className={styles.value}>1234568</div>
-          </DivRow>
-        </DivColumn>
-
-        <div className={styles.header}>PRODUCTS ORDERED</div>
-
-        <DataTable
-          columns={columns}
-          customStyles={customStyles}
-          data=""
-          style={{ minHeight: 200 }}
-          noHeader={true}
-        />
-
-        <HorizontalBorder />
-        <DivRow className={styles.address_container}>
-          <DivColumn className={styles.address_item_container}>
-            <div className={styles.title}>SHIPPING ADDRESS</div>
-            <div className={styles.description}>
-              <span>
-                Building 43B 4th Floor, Suite 402 Street Number 3 P.O. Box 593
-                Kuwait Safat 13006
-              </span>
-            </div>
+            render={({ handleSubmit, form, submitting, pristine, values }) => (
+              <form className={styles.form_container} onSubmit={handleSubmit}>
+                <Field name="pickupDate">
+                  {({ input, meta }) => (
+                    <DatePicker
+                      selected={startDate}
+                      onChange={(date) => {
+                        this.setState({ startDate: date });
+                        input.onChange(date.valueOf());
+                      }}
+                      minDate={new Date()}
+                      customInput={
+                        <CustomRenderInput meta={meta} input={input} />
+                      }
+                    />
+                  )}
+                </Field>
+                <DivRow className={styles.form_button_container}>
+                  <CapsuleButton type="submit" disabled={submitting}>
+                    Create Request
+                  </CapsuleButton>
+                </DivRow>
+              </form>
+            )}
+          />
+          <div className={styles.header}>CUSTOMER DETAILS</div>
+          <DivColumn className={styles.normal_container}>
+            <DivRow className={styles.title}>
+              Name:{" "}
+              <div className={styles.value}>
+                {order.customer_first_name} {order.customer_last_name}
+              </div>
+            </DivRow>
+            <DivRow className={styles.title}>
+              Email: <div className={styles.value}>{order.customer_email}</div>
+            </DivRow>
           </DivColumn>
-
-          <DivColumn className={styles.address_item_container}>
-            <div className={styles.title}>BILLING ADDRESS</div>
-            <div className={styles.description}>
-              <span>
-                Building 43B 4th Floor, Suite 402 Street Number 3 P.O. Box 593
-                Kuwait Safat 13006
-              </span>
-            </div>
-          </DivColumn>
-
+          <div className={styles.header}>PRODUCTS ORDERED</div>
+          <DataTable
+            columns={columns}
+            customStyles={customStyles}
+            data={order.items}
+            style={{ minHeight: 200 }}
+            noHeader={true}
+          />
           <HorizontalBorder />
-          <DivColumn className={styles.address_item_container}>
-            <div className={styles.title}>SHIPPING METHOD</div>
-            <div className={styles.description}>Flat Rate - Flat Rate</div>
-          </DivColumn>
+          <DivRow className={styles.address_container}>
+            <DivColumn className={styles.address_item_container}>
+              <div className={styles.title}>SHIPPING ADDRESS</div>
+              <div className={styles.description}>
+                <span>
+                  Building 43B 4th Floor, Suite 402 Street Number 3 P.O. Box 593
+                  Kuwait Safat 13006
+                </span>
+              </div>
+            </DivColumn>
 
-          <DivColumn className={styles.address_item_container}>
-            <div className={styles.title}>PAYMENT METHOD</div>
-            <div className={styles.description}>Cash On Delivery</div>
-          </DivColumn>
-        </DivRow>
+            <DivColumn className={styles.address_item_container}>
+              <div className={styles.title}>BILLING ADDRESS</div>
+              <div className={styles.description}>
+                <span>
+                  Building 43B 4th Floor, Suite 402 Street Number 3 P.O. Box 593
+                  Kuwait Safat 13006
+                </span>
+              </div>
+            </DivColumn>
+
+            <HorizontalBorder />
+            <DivColumn className={styles.address_item_container}>
+              <div className={styles.title}>SHIPPING METHOD</div>
+              <div className={styles.description}>Flat Rate - Flat Rate</div>
+            </DivColumn>
+
+            <DivColumn className={styles.address_item_container}>
+              <div className={styles.title}>PAYMENT METHOD</div>
+              <div className={styles.description}>Cash On Delivery</div>
+            </DivColumn>
+          </DivRow>
+        </InitialPageLoader>
       </SectionedContainer>
     );
   }
@@ -179,6 +265,14 @@ const mapStateToProps = (state) => {
 const mapDispathToProps = (dispatch) => {
   return {
     getOrderDetailsAction: bindActionCreators(getOrderDetailsAction, dispatch),
+    createPickupRequestAction: bindActionCreators(
+      createPickupRequestAction,
+      dispatch
+    ),
+    showSuccessFlashMessage: bindActionCreators(
+      showSuccessFlashMessage,
+      dispatch
+    ),
   };
 };
 
