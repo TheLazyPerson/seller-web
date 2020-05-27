6250da1a02c75b91a86d2f61ec0f90f47f76e119
Om Komawar <omkomawar222@gmail.com>
2020-05-27 21:26:19 +0530

    updated date in create shipping request

--- src/utils/formatHelper.js
+++ src/utils/formatHelper.js
@@ -1,18 +1,28 @@
-import moment from 'moment';
+import moment from "moment";
 
 export const initMoment = (timeStamp, formatter = timeFormats.utcTimeStamp) => {
-  return moment.utc(timeStamp, formatter).local()
-}
+  return moment.utc(timeStamp, formatter).local();
+};
 
-export const formatTimeStamp = (timeStamp, formatter = timeFormats.dashedDate) => {
-  return initMoment(timeStamp).format(formatter)
-}
+export const momentToDate = (timeStamp, formatter = timeFormats.date) => {
+  return moment(timeStamp, formatter).local();
+};
+export const formatTimeStamp = (
+  timeStamp,
+  formatter = timeFormats.dashedDate
+) => {
+  return initMoment(timeStamp).format(formatter);
+};
+export const formatToDate = (timeStamp, formatter = timeFormats.date) => {
+  return initMoment(timeStamp).format(formatter);
+};
 
 /* 
 1-04-Dec-18 - 05:04:18 PM
 */
 export const timeFormats = {
-  dayMonthComaYear: 'DD MMM, YYYY',
-  utcTimeStamp: 'YYYY-MM-DD HH:mm:ss [UTC]'
+  dayMonthComaYear: "DD MMM, YYYY",
+  utcTimeStamp: "YYYY-MM-DD HH:mm:ss [UTC]",
+  date: "dd/MM/yyyy",
   //add the rest of the date format for the project and use formatTimestamp with this
-}
+};
