bbd2c4e6001e9154302e998ba175d94d54a31100
Om Komawar <omkomawar222@gmail.com>
2020-05-28 13:47:42 +0530

    updated build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.97e166907cade83c842bf161d49f77ee.js"
+  "/precache-manifest.4b7e364dfd2ba9fc2f78d43dfc3f9313.js"
 );
 
 self.addEventListener('message', (event) => {
