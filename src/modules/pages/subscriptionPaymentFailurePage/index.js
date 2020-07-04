9110ad032a7b1c732c1bdf0bb1341c6786e23669
Akshay Bartakke <bartakke7@gmail.com>
2020-07-04 07:44:31 -0400

    subscription and enroll exhibition rtl changes added

--- src/modules/pages/subscriptionPaymentFailurePage/index.js
+++ src/modules/pages/subscriptionPaymentFailurePage/index.js
@@ -4,25 +4,42 @@ import DivColumn from "CommonComponents/divColumn";
 import styles from "./payment_failure.module.scss";
 import CapsuleButton from "CommonComponents/capsuleButton";
 import navigatorHoc from "Hoc/navigatorHoc";
+import { SignalCellularNull } from "material-ui-icons";
+import translatorHoc from "Hoc/translatorHoc";
+import { connect } from "react-redux";
 
 class SubscriptionPaymentFailurePage extends Component {
   render() {
+    const { translate, isRTL } = this.props;
     return (
       <FullWidthContainer>
         <DivColumn
           verticalCenter
           horizontalCenter
-          className={styles.page_container}
+          className={` ${styles.page_container} ${isRTL ? styles.rtl : ""}`}
         >
-          <div className={styles.title}>Payment Failed</div>
-          <div className={styles.description}>
-            There was problem while accepting your payment.
+          <div className={styles.title}>
+            {translate("exhibition_overview.subscription_success")}
           </div>
-          <CapsuleButton>Retry</CapsuleButton>
+          <div className={styles.description}>
+            {translate("exhibition_overview.description")}
+          </div>
+          <CapsuleButton>
+            {translate("exhibition_overview.retry")}
+          </CapsuleButton>
         </DivColumn>
       </FullWidthContainer>
     );
   }
 }
 
-export default navigatorHoc(SubscriptionPaymentFailurePage);
+const mapStateToProps = (state) => {
+  return {
+    isRTL: state.languageReducer.isRTL,
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  null
+)(navigatorHoc(translatorHoc(SubscriptionPaymentFailurePage)));
--- src/modules/pages/subscriptionPaymentSuccessPage/index.js
+++ src/modules/pages/subscriptionPaymentSuccessPage/index.js
@@ -20,20 +20,31 @@ class SubscriptionPaymentSuccessPage extends Component {
     clearTimeout(this.id);
   }
   render() {
+    const { translate, isRTL } = this.props;
     return (
       <FullWidthContainer>
         <DivColumn
           verticalCenter
           horizontalCenter
-          className={styles.page_container}
+          className={` ${styles.page_container} ${isRTL ? styles.rtl : ""}`}
         >
-          <div className={styles.title}>Payment Successful</div>
-          <div className={styles.description}>Welcome to the family.</div>
+          <div className={styles.title}>
+            {translate("exhibition_overview.payment1")}
+          </div>
+          <div className={styles.description}>
+            {translate("exhibition_overview.description1")}.
+          </div>
         </DivColumn>
       </FullWidthContainer>
     );
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    isRTL: state.languageReducer.isRTL,
+  };
+};
 
 const mapDispathToProps = (dispatch) => {
   return {
@@ -45,6 +56,6 @@ const mapDispathToProps = (dispatch) => {
 };
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispathToProps
 )(navigatorHoc(SubscriptionPaymentSuccessPage));
