295b4e7a1b82b1a3d0020fb36ab1078cd94f3ded
Om Komawar <omkomawar222@gmail.com>
2020-07-04 08:49:38 +0530

    added translations for sales page

--- src/modules/pages/salesPage/index.js
+++ src/modules/pages/salesPage/index.js
@@ -12,62 +12,64 @@ import DataTableContainer from "CommonContainers/dataTableContainer";
 import { getTransactionListAction } from "Core/modules/transaction/transactionActions";
 import InitialPageLoader from "CommonContainers/initialPageLoader";
 import Button from "@material-ui/core/Button";
+import translatorHoc from "Hoc/translatorHoc";
 
 class SalesPage extends Component {
-  columns = memoize(() => [
-    {
-      name: "ID",
-      selector: "id",
-      sortable: true,
-    },
-    {
-      name: "TRANSACTION ID",
-      selector: "transaction_id",
-      sortable: true,
-    },
-    {
-      name: "COMMENT",
-      selector: "comment",
-      sortable: true,
-      grow: 2,
-    },
-    {
-      name: "GRAND TOTAL",
-      selector: "order.grand_total",
-      sortable: true,
-    },
-    {
-      cell: (value) => (
-        <Button
-          variant="contained"
-          color="primary"
-          className={styles.custom_button}
-          onClick={() => {
-            const { navigateTo } = this.props;
-            navigateTo("sales-details", { transactionId: value.id });
-          }}
-        >
-          View
-        </Button>
-      ),
-      button: true,
-    },
-  ]);
   render() {
     const {
       transactionReducer: { transactionList },
       getTransactionListAction,
+      translate,
     } = this.props;
+    const columns = memoize(() => [
+      {
+        name: `${translate("sales_list.table.id")}`,
+        selector: "id",
+        sortable: true,
+      },
+      {
+        name: `${translate("sales_list.table.transaction_id")}`,
+        selector: "transaction_id",
+        sortable: true,
+      },
+      {
+        name: `${translate("sales_list.table.comment")}`,
+        selector: "comment",
+        sortable: true,
+        grow: 2,
+      },
+      {
+        name: `${translate("sales_list.table.grand_total")}`,
+        selector: "order.grand_total",
+        sortable: true,
+      },
+      {
+        cell: (value) => (
+          <Button
+            variant="contained"
+            color="primary"
+            className={styles.custom_button}
+            onClick={() => {
+              const { navigateTo } = this.props;
+              navigateTo("sales-details", { transactionId: value.id });
+            }}
+          >
+            {translate("sales_list.table.view_button")}
+          </Button>
+        ),
+        button: true,
+      },
+    ]);
     return (
       <SectionedContainer sideBarContainer={<SideNav />}>
         <DivColumn fillParent className={styles.sales_page_container}>
-          <NavHeader title="Transactions"></NavHeader>
+          <NavHeader title={translate("sales_list.title")}></NavHeader>
           <InitialPageLoader initialPageApi={getTransactionListAction}>
             <DivColumn fillParent className={styles.content_container}>
               <DataTableContainer
                 data={transactionList}
-                title="Transactions"
-                columns={this.columns()}
+                title={translate("sales_list.title")}
+                columns={columns()}
               />
             </DivColumn>
           </InitialPageLoader>
@@ -95,4 +97,4 @@ const mapDispathToProps = (dispatch) => {
 export default connect(
   mapStateToProps,
   mapDispathToProps
-)(navigatorHoc(SalesPage));
+)(translatorHoc(navigatorHoc(SalesPage)));
