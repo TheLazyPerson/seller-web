c28eb0bd6ffe9a98d78fdcb12964971ce38eb9ab
Om Komawar <omkomawar222@gmail.com>
2020-07-15 08:55:42 +0530

    edit product cancel button fix

--- src/modules/pages/productsPage/editProduct/index.js
+++ src/modules/pages/productsPage/editProduct/index.js
@@ -71,7 +71,8 @@ class EditProduct extends Component {
   };
 
   onClickCancel = () => {
-    this.onBackPress();
+    const { navigateTo } = this.props;
+    navigateTo("products");
   };
 
   onSelectCategory = (data) => {
@@ -442,7 +443,7 @@ class EditProduct extends Component {
                       );
                     })}
                     <DivRow className={styles.form_button_container}>
-                      <SecondaryCapsuleButton onClick={onClickCancel}>
+                      <SecondaryCapsuleButton onClick={this.onClickCancel}>
                         {translate("edit_product.cancel")}
                       </SecondaryCapsuleButton>
                       <CapsuleButton type="submit" disabled={submitting}>
